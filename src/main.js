d874a52b6e27d4234c05ea08eccc51c74bef4ed1
xinchen2019 <1312837959@qq.com>
2023-05-07 21:00:52 +0800

    主页完善

--- src/main.js
+++ src/main.js
@@ -2,6 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './plugins/pxrem.js'
+
+import axios from 'axios'
+
+// 导入NProgress, 包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 在request 拦截器中, 展示进度条 NProgress.start()
+// 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  // console.log(config)
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+// response 拦截器中,  隐藏进度条NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
+
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+// 路由懒加载
+const Login = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Login.vue')
+const Home = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Home.vue')
+const Welcome = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Welcome.vue')
+
+const Users = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/user/User.vue')
 
 Vue.use(VueRouter)
 
 const routes = [
+  // 重定向路由
+  { path: '/', redirect: '/login' },
+  // 当用户访问 /login 这个地址时，通过 component 属性指定要展示的组件 Login
+  { path: '/login', component: Login },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component: Home,
+    redirect: '/Welcome',
+    children: [
+      { path: '/home', component: Welcome },
+      { path: '/users', component: Users },
+      // { path: '/rights', component: Rights },
+      // { path: '/roles', component: Roles },
+      // { path: '/categories', component: Cate },
+      // { path: '/params', component: Params },
+      // { path: '/goods', component: GoodsList },
+      // { path: '/goods/add', component: Add },
+      // { path: '/orders', component: Order },
+      // { path: '/reports', component: Report }
+    ]
   }
 ]
 
